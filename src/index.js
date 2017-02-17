e8744ec2681d0446ba603249ead905cab5dd2a5a
TomaMajic <tomamajic01@gmail.com>
2017-02-17 15:49:49 +0100

    search1

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
+import SearchBar from './components/search_bar'; // Cili path ako je nas exportani file
+
+const API_KEY = 'AIzaSyCUiin13ljpY6V8PhrN7HsY5WJJFWg7LyE';
 
 
 // Create a new component.
 // This component should produce some HTML.
 const App = () => {
-  return <div>Hi with jQuery!</div>;
+  return (
+  	<div>
+  		<SearchBar />
+  	</div>
+  );
 }
 
 // Put this component into the DOM
 ReactDOM.render(<App />, $('.container')[0]);
+
